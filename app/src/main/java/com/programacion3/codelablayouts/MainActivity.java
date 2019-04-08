c9d1125eebbdee33f66787cce196a18ef1e49684
Adrian Rodriguez <adrian.rodriguez@mojix.com>
2019-04-08 10:10:00 -0400

    Added EditorAction listener

--- app/src/main/java/com/programacion3/codelablayouts/MainActivity.java
+++ app/src/main/java/com/programacion3/codelablayouts/MainActivity.java
@@ -2,6 +2,10 @@ package com.programacion3.codelablayouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.editTextPassword);
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                Toast.makeText(MainActivity.this, "EDITOR ACTION LISTENER", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
